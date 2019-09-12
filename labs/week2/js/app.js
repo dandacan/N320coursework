c4993093f3d7b6b17b58c9404116aaad33588171
dandacan <dknintendowii20@gmail.com>
2019-09-12 16:28:34 -0400

    actually staged all the changes for the other files instead of the one

--- labs/week2/js/app.js
+++ labs/week2/js/app.js
@@ -1,39 +1,96 @@
-class Cloud{
-	constructor(){
-		this.drops = [];
-
-	}
-
-	//create rain drops
-	createDrop(){
-
-	}
-}
 class Drop{
+	//once created,
 	constructor(){
-		this.x = 40;
-		this.y = 0;
+		//randomize the x and y position, as well as set "done" to false
+		this.x = Math.random() * 800;
+		this.y = Math.random() * 40;
+		this.done = false;
 	}
 	update(){
+		//increase the value of y to move the thing down
 		this.y ++;
-		fill(0,0,5);
+		//if above the ground, draw it
+		if(this.y < 500){
+			this.Fill();
+			// otherwise, set "done" to true
+		}else if(this.y >= 500){
+			this.done = true;
+		}
+	}
+	Fill(){
+		//set a color to render as
+		fill(0,0,200);
+		//create a circle where it is
 		circle(this.x, this.y, 5);
 	}
 
 }
 
+class Ground{
+	constructor(){
+		//set to and from points for lerping, as well as how far it is
+		this.colorBase = 0;
+		this.colorTo = 255;
+		this.colorPercent = 0.05;
+	}
+	update(){
+		//set the color mode to RGB just to be sure
+		colorMode(RGB);
+		//set the color to a lerp from start to end, and color%
+		fill(lerpColor(color(50,50,this.colorBase, 200), color(0,0,this.colorTo, 255), this.colorPercent));
+		//draw a rectangle
+		rect(0, 500, 800, 100);
+	}
+}
 
-var d = new Drop();
+class Cloud{
+	//take ground in the constructor and declare variables
+	constructor(ground){
+		this.drops = [];
+		this.ground = ground;
+	}
+
+	//create rain drops
+	createDrop(){
+		this.drops.push(new Drop());
+	}
+
+	update(){
+		//for every drop...
+		for (var i = this.drops.length - 1; i >= 0; i--) {
+			//update the drops
+			this.drops[i].update();
+			//check if its done, and if so, move the ground color up by 5%, as well as delete the drop
+			if(this.drops[i].done == true){
+				this.drops.splice(i, 1);
+				this.ground.colorPercent += 0.05;
+			}
+		}
+		//also update the ground
+		this.ground.update();
+	}
+}
+
+//instatiate 'em
+var ground = new Ground();
+var cloud = new Cloud(ground);
 
 //before it starts, this is called
 function setup() {
-	createCanvas(400,300);
+	createCanvas(800,600);
 }
 
 
 //most pcs will call this 60 times a second
 function draw(){
-	d.update();
-
+	//redraw the background as white in RGB
+	colorMode(RGB);
+	background(255);
+	//5% of frames will spawn a drop
+	if (Math.random() < 0.05){
+		cloud.createDrop();
+	}
+	//update the stuffs
+	cloud.update();
 }
 
--- labs/week3/js/app.js
+++ labs/week3/js/app.js
@@ -1,3 +1,4 @@
+//subject
 class Ball {
   //construct with base position and velocity
     constructor() {
@@ -5,48 +6,59 @@ class Ball {
       this.velocity = { x: 10, y: 0 };
     }
     
+    //once a frame,
     update() {
-      
+      //change the position according to velocity
       this.position.x += this.velocity.x;
       this.position.y += this.velocity.y;
-      
+      //draw it
       circle(this.position.x, this.position.y, 20);
-      
+      //call the ball beyond function
       if(this.position.x < 0 || this.position.x > 400) {
         World.ballBeyond(this);
       }
     }
     
   }
+//listener
 class Box {
-
+  //when created, position can be set, and the size is always the same when created
   constructor(x, y) {
     this.position = { x: x, y: y };
     this.size = {x: 5, y:5};
   }
-
+  //once a frame, draw the function
   update() {
       fill("#FFF");
       rect(this.position.x, this.position.y, this.size.x, this.size.y);
-    }
-    grow(){
-      this.size.x += 5;
-      this.size.y += 5;
-    } 
+  }
+  //increase the size by 5 in both x and y
+  grow(){
+    this.size.x += 5;
+    this.size.y += 5;
+  } 
 }
+//mediator/manager
 var World = {
+  //set the background color
   bgcolor: [237, 119, 83],
+  //declare place for boxes to go
   boxes: [],
-   update: function(){
+  //call update in all boxes
+  update: function(){
     for (var i = this.boxes.length - 1; i >= 0; i--) {
       this.boxes[i].update();
     }
   },
+  //called when ball hits wall
   ballBeyond: function(whichBall) {
+    //randomize color
     this.bgcolor = [ Math.random()*255, Math.random()*255, 83 ];
+    //grow the boxes
     for (var i = this.boxes.length - 1; i >= 0; i--) {
       this.boxes[i].grow();
     }
+    //reset position and randomize speed and direction
     whichBall.position.x = 100;
     whichBall.velocity.x = (Math.random() - .5) * 20;
   }
@@ -54,18 +66,22 @@ var World = {
 
 
 
-
+//create boxes and ball
 var ball = new Ball();
 World.boxes.push(new Box(0,0));
 World.boxes.push(new Box(200,200));
 
+//before first frame,
 function setup() {
+  //create a canvas for our objects
   createCanvas(400,300);
   
 }
-
+//every frame,
 function draw() {
+  //update the background
   background( World.bgcolor );
+  //call all update functons in our classes
   ball.update();
   World.update();
 }
